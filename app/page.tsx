6000dca653e893b52b6180f36a1fc1ef22f2d4e0
djgomezbe <jkg@outlook.be>
2025-01-27 22:18:16 +0100

    added landing page of fodmap

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,49 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { Button } from "../components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <div className="mx-auto max-w-2xl py-32 text-white sm:py-48 lg:py-56">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="relative rounded-full px-3 py-1 text-sm/6 text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            Onze app launched op 20 April 2025!.
+            <a href="fodmap.html" className="font-semibold text-green-600">
+              <span className="absolute inset-0" aria-hidden="true"></span>
+              Lees meer
+              <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+        <div className="text-center">
+          <h1 className="text-balance text-5xl font-semibold tracking-tight text-white sm:text-7xl">
+            Jouw Partner in een Gezond FODMAP-dieet!
+          </h1>
+          <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            Heb je last van spijsverteringsproblemen of volg je een
+            FODMAP-dieet? Maak kennis met Fodmap Tracker, dé app die het
+            eenvoudiger maakt om je dieet te beheren en je gezondheid te
+            verbeteren.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Button asChild size="sm" variant={"default"}>
+              <Link href="/sign-in">Sign in</Link>
+            </Button>
+            <Button asChild size="sm" variant={"default"}>
+              <Link href="/sign-up">Sign Up</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+      </div>
     </>
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,11 @@
 import NextLogo from "./next-logo";
 import SupabaseLogo from "./supabase-logo";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer"></a>
-      </div>
-      <h1 className="sr-only">Fodmap-Tracker</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Your partner during the Fodmap-diet.
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    <div>
+      <div>Fodmap-Tracker</div>
     </div>
   );
 }

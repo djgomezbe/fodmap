5e6235fb11951ccda4940de1d66104471c9d05e1
djgomezbe <jkg@outlook.be>
2025-01-28 07:24:02 +0100

    completing the template

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import Image from "./fodmap.png";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -41,8 +42,8 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
+                  <div className="flex gap-5 items-center text-2xl font-semibold">
+                    <Link href={"/"}>Fodmap-Tracker</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>

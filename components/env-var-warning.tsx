cec5c0777b5ee6e344033f2edc37b4f32092e029
djgomezbe <jkg@outlook.be>
2025-02-08 12:45:01 +0100

    fix auth buttons small screen

--- components/env-var-warning.tsx
+++ components/env-var-warning.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export function EnvVarWarning() {
   return (
-    <div className="flex gap-4 items-center">
+    <div className="flex gap-4 items-center max-xl:hidden">
       <div className="flex gap-2">
         <button className="text-green-600 hover:text-white border border-green-600 bg-white hover:bg-green-600/90 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-green-500 dark:bg-black dark:text-white dark:hover:text-white dark:hover:bg-green-500  dark:focus:ring-green-800">
           <Link href="/sign-in">Aanmelden</Link>
